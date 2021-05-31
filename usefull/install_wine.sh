2ee0657fad44ae34d998d40c260729e8d1907970
Abrahao <abrahaobittar@gmail.com>
2021-05-31 16:14:34 -0300

    Novos scripts, install wine, client oracle

---
+++ usefull/install_wine.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Testado em Ubuntu20.04 x86_x64
+sudo dpkg --add-architecture i386 && sudo apt update
+wget -qO- https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
+sudo apt install software-properties-common
+sudo apt-add-repository "deb http://dl.winehq.org/wine-builds/ubuntu/ $(lsb_release -cs) main"
+sudo apt install --install-recommends winehq-stable -y
+wine --version
