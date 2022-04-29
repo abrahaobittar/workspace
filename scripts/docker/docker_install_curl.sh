fa3d73500e5c5fd1a11f0a7ded3349d04c7fff84
Abrahao Bittar <suporte2@tecnixinfo.com.br>
2022-04-29 14:40:48 -0300

    syntax correction

--- scripts/docker/docker_install_curl.sh
+++ scripts/docker/docker_install_curl.sh
@@ -2,3 +2,5 @@
 echo 123 | sudo -S curl -fsSL https://get.docker.com | bash
 echo 123 | sudo -S dnf install docker-compose -y
 echo 123 | sudo -S usermod -aG docker $USER
+echo 123 | sudo -S systemctl enable docker
+echo 123 | sudo -S systemctl start docker
