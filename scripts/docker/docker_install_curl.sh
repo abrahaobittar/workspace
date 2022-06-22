1df59525759a8e14553066391c37473274fa7c9e
Abrahao <abrahaobittar@gmail.com>
2022-06-22 19:25:01 -0300

    script refactoring

--- scripts/docker/docker_install_curl.sh
+++ scripts/docker/docker_install_curl.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-cd /home/abrahao/Code
 sudo -S 123 curl -fsSL https://get.docker.com | bash
 sudo -S 123 usermod -aG docker $USER
-sudo -S 123 apt install docker-compose
+sudo -S 123 dnf install docker-compose
