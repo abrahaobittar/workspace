9ec941ec4866905b80e3665661809e5714e21085
Abrahao Bittar <suporte2@tecnixinfo.com.br>
2022-07-26 12:57:30 -0300

    script to debloat linux mint

---
+++ usefull/debloat_mint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Deblot linux Mint 21
+sudo apt remove transmission-gtk  celluloid hypnotix hexchat libreoffice webapp-manager \
+thunderbird rhythmbox pix drawing simple-scan mintwelcome mintbackup sticky seahorse onboard \
+thingy xfce4-dict xfce4-screenshooter timeshift -y
+sudo apt-get remove --purge libreoffice* -y
+sudo apt-get clean && sudo apt autoremove -y
