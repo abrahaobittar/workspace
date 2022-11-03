872af45f29f32a1d1aaed6da8cc62f65982f5479
Abrahao Bittar <suporte2@tecnixinfo.com.br>
2022-11-03 17:51:51 -0300

    new folder organization and debloat script

--- scripts/linux/debloat_mint.sh
+++ scripts/linux/debloat_mint.sh
@@ -4,5 +4,7 @@
 sudo apt remove transmission-gtk  celluloid hypnotix hexchat libreoffice webapp-manager \
 thunderbird rhythmbox pix drawing simple-scan mintwelcome mintbackup sticky seahorse onboard \
 thingy xfce4-dict xfce4-screenshooter timeshift redshift-gtk warpinator -y
+
 sudo apt-get remove --purge libreoffice* -y
+
 sudo apt-get clean && sudo apt autoremove -y
