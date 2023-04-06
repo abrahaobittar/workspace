61ed8f9b8eb7ab4ad5c3e6886859b1640fb9c632
Abrahao Bittar <suporte2@tecnixinfo.com.br>
2023-04-06 11:25:10 -0300

    Fixing script install

--- scripts/docker/docker_install_curl.sh
+++ scripts/docker/docker_install_curl.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-<<<<<<< HEAD
-echo 123 | sudo -S curl -fsSL https://get.docker.com | bash
-echo 123 | sudo -S dnf install docker-compose -y
-echo 123 | sudo -S usermod -aG docker $USER
-echo 123 | sudo -S systemctl enable docker
-echo 123 | sudo -S systemctl start docker
-=======
-sudo -S 123 curl -fsSL https://get.docker.com | bash
-sudo -S 123 usermod -aG docker $USER
-sudo -S 123 dnf install docker-compose
->>>>>>> 3cce2f8870d4189c7917bdba52297bd013e88ce2
+
+if [ "$UID" -ne 0 ]; then
+    echo "You must be root to run this script"
+    exit 1
+fi
+
+curl -fsSL https://get.docker.com | bash
+dnf install docker-compose -y
+usermod -aG docker $USER
+systemctl enable docker
+systemctl start docker
